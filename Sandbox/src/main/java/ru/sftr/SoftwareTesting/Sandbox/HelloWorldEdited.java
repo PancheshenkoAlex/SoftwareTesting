e53df59f440ec38fb131e3139f228ac95d3f4235
PancheshenkoAlex <41270463+PancheshenkoAlex@users.noreply.github.com>
2019-03-03 21:14:09 +0100

    package

--- Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/HelloWorldEdited.java
+++ Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/HelloWorldEdited.java
@@ -3,7 +3,7 @@ package ru.sftr.SoftwareTesting.Sandbox;
 public class HelloWorldEdited {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello, World");
     }
 
