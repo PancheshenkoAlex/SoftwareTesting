169acc328d7ca67d167772a7046c94562f83a0cb
PancheshenkoAlex <41270463+PancheshenkoAlex@users.noreply.github.com>
2019-03-04 20:37:22 +0100

    Area function was added with calculation Square area and Rectangle area

--- Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/HelloWorldEdited.java
+++ Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/HelloWorldEdited.java
@@ -4,7 +4,28 @@ public class HelloWorldEdited {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World");
+        hello("Alex");
+        hello("world");
+        double l = 1.2;
+        double a = 2, b = 3;
+        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+        System.out.println("Площадь прямоугольника со стороной а = " + a + " и b = " + b + " равна " + area(a, b))
+        ;
     }
+
+    public static void hello(String name) {
+        System.out.println("Hello" + ' ' + name + "!");
+    }
+
+    public static double area(double len) {
+        double areaSquare = len * len;
+        return areaSquare;
+    }
+
+    public static double area(double a, double b){
+        double areaRectangle = a * b;
+        return areaRectangle;
+    }
+
 
 }
