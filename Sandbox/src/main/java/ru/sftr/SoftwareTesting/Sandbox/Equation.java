300c40eb2df966e1c7862c267a89075ff92de35f
PancheshenkoAlex <41270463+PancheshenkoAlex@users.noreply.github.com>
2019-03-20 21:23:20 +0100

    Equation tests creation in Sandbox

--- Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/Equation.java
+++ Sandbox/src/main/java/ru/sftr/SoftwareTesting/Sandbox/Equation.java
@@ -15,14 +15,7 @@ public class Equation {
 
         double d = b * b - 4 * a * c;
 
-        if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                    n = -1;
-                }
-            }
-            n = 1;
-        } else {
+        if (a != 0) {
             if (d > 0) {
                 n = 2;
             } else if (d == 0) {
@@ -31,9 +24,18 @@ public class Equation {
                 n = 0;
             }
 
-        }
+        } else if (b != 0) {
+            n = 1;
 
+        } else if (c != 0) {
+            n = 0;
+            
+        } else {
+
+            n = -1;
+        }
     }
+
 
     public int rootNumber() {
         return n;
