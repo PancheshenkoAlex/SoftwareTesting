1a248f6918f3f367879e14cd44fae69408896784
PancheshenkoAlex <41270463+PancheshenkoAlex@users.noreply.github.com>
2019-03-31 21:40:09 +0200

    Added 2 variants of isPrimesFast test

--- Sandbox/src/test/java/ru/sftr/SoftwareTesting/Sandbox/PrimeTests.java
+++ Sandbox/src/test/java/ru/sftr/SoftwareTesting/Sandbox/PrimeTests.java
@@ -9,14 +9,23 @@ public class PrimeTests {
     public void testPrimes() {
         Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
     }
+    @Test
+    public void testPrimesFast() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
 
     @Test
+    public void testPrimesFastSecond() {
+        Assert.assertTrue(Primes.isPrimeFastSecond(Integer.MAX_VALUE));
+    }
+
+    @Test(enabled = false)
     public void testPrimesLong() {
         long n = Integer.MAX_VALUE;
         Assert.assertTrue(Primes.isPrime(n));
     }
 
-    @Test(enabled = false)
+    @Test
     public void testNonPrimes() {
         Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
     }
