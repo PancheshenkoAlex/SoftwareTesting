f15fe98fbaed71f2a413d5c947b21179d5f192fc
PancheshenkoAlex <41270463+PancheshenkoAlex@users.noreply.github.com>
2019-04-22 18:13:15 +0200

    Lesson 5.6. Asserts update

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveGroupTests.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-import java.util.Set;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class RemoveGroupTests extends TestBase {
 
@@ -21,14 +23,13 @@ public class RemoveGroupTests extends TestBase {
     @Test
     public void testRemoveGroup() throws Exception {
 
-        Set<GroupData> before = app.group().all();
-        GroupData deleteGroup = before.iterator().next();
-        app.group().deleteSelected(deleteGroup);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        Groups before = app.group().all();
+        GroupData deletedGroup = before.iterator().next();
+        app.group().delete(deletedGroup);
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() - 1));
+        assertThat(after, equalTo(before.without(deletedGroup)));
 
-        before.remove(deleteGroup);
-        Assert.assertEquals(before, after);
 
     }
 
